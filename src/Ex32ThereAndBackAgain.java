671429b38bc873ba82a462b16ca42ddbd807b372
Eimear Reilly <eimearreilly@uk-gn-38044.local>
2017-02-01 09:38:31 +0000

    9th commit

--- src/Ex32ThereAndBackAgain.java
+++ src/Ex32ThereAndBackAgain.java
@@ -1,5 +1,22 @@
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 /**
  * Created by eimearreilly on 27/01/2017.
  */
+// CAMEL CASE YO!!
+// there wasn't really an exercise of any substance for this class
+
 public class Ex32ThereAndBackAgain {
+    public static void main(String[] args){
+        System.out.println("Here. ");
+        erebor();
+        System.out.println("Here. ");
+        // erebor();
+        System.out.println("Here. ");
+        erebor();
+    }
+
+    public static void erebor(){
+        System.out.println("There. ");
+    }
 }
---
+++ src/Ex56TemperatureSample.java
@@ -0,0 +1,5 @@
+/**
+ * Created by eimearreilly on 01/02/2017.
+ */
+public class Ex56TemperatureSample {
+}
